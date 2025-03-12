fc8fce541326551cba321c0192cf0d45b9795e80
Priyanka Shivaji Kadam <142817932+PriyankaShivajiKadam@users.noreply.github.com>
2025-03-12 15:33:25 +0530

    Add files via upload

---
+++ dowhile_1to10.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class dowhile_1to10 {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+		
+
+	}
+
+}
---
+++ while_decreamentby4.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class while_decreamentby4 {
+
+	public static void main(String[] args) {
+		int i=11;
+		while(i>=-33) {
+			System.out.println(i);
+			i-=4;
+		}
+
+	}
+
+}
